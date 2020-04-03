881a48766848126db138651bfb857840e2d570a1
tigerabrodi <fahdabrodi@gmail.com>
2020-04-03 18:18:58 +0200

    Changed the touchs section

--- src/components/home/touch/Touch.js
+++ src/components/home/touch/Touch.js
@@ -7,7 +7,7 @@ const Touch = () => {
         <div className="touch" data-aos-duration="1300" data-aos="zoom-in">
         <div className="touch__wrapper">
         <h1 className="touch__heading">Get In Touch</h1>
-        <p className="touch__text">My inbox is always open, feel free to ask me anything, I'll try my best to answer your email!</p>
+        <p className="touch__text">I am currently looking for a full-time position as a Front-End Developer. My inbox is always open, feel free to ask me anything, I'll try my best to answer your email!</p>
         <Link to="/contact" className="touch__button">Get In Touch</Link>
         </div>
         </div>
